9f9f898fea181a103c43e344785ce72e1916f5a8
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-04-22 17:28:22 +0200

    attempted fix for elastix and transformix for mac users

--- src/main/java/ch/epfl/biop/wrappers/elastix/Elastix.java
+++ src/main/java/ch/epfl/biop/wrappers/elastix/Elastix.java
@@ -3,6 +3,8 @@ package ch.epfl.biop.wrappers.elastix;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import ij.IJ;
 import ij.Prefs;
 
 /**
@@ -26,12 +28,61 @@ public class Elastix {
             (System.getProperty("os.name")
                     .startsWith("Windows") ? "NUL" : "/dev/null")
     );
-    
+
     public static void execute(List<String> options, boolean verbose) throws IOException, InterruptedException {
             //options.forEach(s -> System.out.println(s));
             List<String> cmd = new ArrayList<>();
-            cmd.add(exePath);
-            cmd.addAll(options);
+
+            if (IJ.isMacOSX() || IJ.isLinux())
+            {
+                // Modified from Christian Tischer:
+                // https://github.com/tischi/DEPRECATED-fiji-plugin-elastixWrapper/blob/d02bb91820d5912c68cc31b6e562f7d6e5d14145/src/main/java/de/embl/cba/elastixwrapper/elastix/ElastixWrapper.java#L563
+                // We assume a folder hierarchy
+                // elastix
+                //  |
+                //  |-bin
+                //  |   |-elastix
+                //  |   \-transformix
+                //  \-lib
+                //      |-libANNlib
+                // And defaultExePath points to the 'elastix' file
+                //
+                String elastix_folder_path= new File(new File(exePath).getParent()).getParent();
+                String registrationCommand = "";
+                for (String command: options) {
+                    registrationCommand+=" "+command;
+                }
+
+                cmd.add("bash");
+                cmd.add("-c");
+
+                String exportPATH = "export PATH="+elastix_folder_path+"/bin/:$PATH";
+
+                String exportLIB="";
+                if ( IJ.isMacOSX() )
+                {
+                    exportLIB = "export DYLD_LIBRARY_PATH="+elastix_folder_path+"/lib/:$DYLD_LIBRARY_PATH";
+
+                }
+                else if ( IJ.isLinux() )
+                {
+                    // Not tested for linux
+                    exportLIB = "export LD_LIBRARY_PATH="+elastix_folder_path+"/lib/:$LD_LIBRARY_PATH";
+                }
+
+                cmd.add(
+                        exportLIB+" ; "+
+                        exportPATH+" ; "+
+                        "elastix"+registrationCommand
+                );
+
+            } else if (IJ.isWindows()) {
+                cmd.add(exePath);
+                cmd.addAll(options);
+            } else {
+                System.err.println("Elastix error: type of OS not found.");
+            }
+
             ProcessBuilder pb = new ProcessBuilder(cmd);
             pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
             if (verbose) {
--- src/main/java/ch/epfl/biop/wrappers/transformix/Transformix.java
+++ src/main/java/ch/epfl/biop/wrappers/transformix/Transformix.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+import ij.IJ;
 import ij.Prefs;
 
 public class Transformix {	
@@ -32,18 +33,67 @@ public class Transformix {
     );
 
     static void execute(List<String> options, boolean verbose)  throws IOException, InterruptedException {
-            List<String> cmd = new ArrayList<>();
+        //options.forEach(s -> System.out.println(s));
+        List<String> cmd = new ArrayList<>();
+
+        if (IJ.isMacOSX() || IJ.isLinux())
+        {
+            // Modified from Christian Tischer:
+            // https://github.com/tischi/DEPRECATED-fiji-plugin-elastixWrapper/blob/d02bb91820d5912c68cc31b6e562f7d6e5d14145/src/main/java/de/embl/cba/elastixwrapper/elastix/ElastixWrapper.java#L563
+            // We assume a folder hierarchy
+            // elastix
+            //  |
+            //  |-bin
+            //  |   |-elastix
+            //  |   \-transformix
+            //  \-lib
+            //      |-libANNlib
+            // And defaultExePath points to the 'elastix' file
+            //
+            String transformix_folder_path= new File(new File(exePath).getParent()).getParent();
+            String transformationCommand = "";
+            for (String command: options) {
+                transformationCommand+=" "+command;
+            }
+
+            cmd.add("bash");
+            cmd.add("-c");
+
+            String exportPATH = "export PATH="+transformix_folder_path+"/bin/:$PATH";
+
+            String exportLIB="";
+            if ( IJ.isMacOSX() )
+            {
+                exportLIB = "export DYLD_LIBRARY_PATH="+transformix_folder_path+"/lib/:$DYLD_LIBRARY_PATH";
+
+            }
+            else if ( IJ.isLinux() )
+            {
+                // Not tested for linux
+                exportLIB = "export LD_LIBRARY_PATH="+transformix_folder_path+"/lib/:$LD_LIBRARY_PATH";
+            }
+
+            cmd.add(
+                    exportLIB+" ; "+
+                    exportPATH+" ; "+
+                    "transformix"+transformationCommand
+            );
+
+        } else if (IJ.isWindows()) {
             cmd.add(exePath);
             cmd.addAll(options);
-            ProcessBuilder pb = new ProcessBuilder(cmd);
-            if (verbose) {
-                pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-            } else {
-                pb.redirectOutput(NULL_FILE);
-            }
-            pb.redirectError(ProcessBuilder.Redirect.INHERIT);
-            Process p = pb.start();
-            p.waitFor();
+        } else {
+            System.err.println("Transformix error: type of OS not found.");
+        }
+        ProcessBuilder pb = new ProcessBuilder(cmd);
+        if (verbose) {
+            pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        } else {
+            pb.redirectOutput(NULL_FILE);
+        }
+        pb.redirectError(ProcessBuilder.Redirect.INHERIT);
+        Process p = pb.start();
+        p.waitFor();
     }
     
     public static void execute(String singleCommand) throws IOException, InterruptedException {
