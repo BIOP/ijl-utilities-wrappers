8b7305eb82f6c9c53781af244576c6ea723f7f3c
Romain Guiet <romain.guiet@epfl.ch>
2021-10-29 08:08:12 +0200

    removes extralines and reformat code

--- src/main/java/ch/epfl/biop/wrappers/cellpose/CellposeTaskSettings.java
+++ src/main/java/ch/epfl/biop/wrappers/cellpose/CellposeTaskSettings.java
@@ -7,13 +7,13 @@ public class CellposeTaskSettings {
 
     String datasetDir;
     String model;
-    int ch1 ;
-    int ch2 =-1 ;
+    int ch1;
+    int ch2 = -1;
 
     // value defined from https://cellpose.readthedocs.io/en/latest/api.html
-    int diameter = 30 ;
-    double flow_threshold = 0.4 ;
-    double cellprob_threshold = 0.0 ;
+    int diameter = 30;
+    double flow_threshold = 0.4;
+    double cellprob_threshold = 0.0;
     //double anisotropy = 1 ;
 
     boolean use3D;
@@ -29,26 +29,27 @@ public class CellposeTaskSettings {
         return this;
     }
 
-    public CellposeTaskSettings setDatasetDir( String datasetDir) {
+    public CellposeTaskSettings setDatasetDir(String datasetDir) {
         this.datasetDir = datasetDir;
         return this;
     }
 
-    public CellposeTaskSettings setModel( String model) {
+    public CellposeTaskSettings setModel(String model) {
         this.model = model;
         return this;
     }
 
-    public CellposeTaskSettings setChannel1( int ch1) {
+    public CellposeTaskSettings setChannel1(int ch1) {
         this.ch1 = ch1;
         return this;
     }
 
-    public CellposeTaskSettings setChannel2( int ch2) {
+    public CellposeTaskSettings setChannel2(int ch2) {
         this.ch2 = ch2;
         return this;
     }
-    public CellposeTaskSettings setDiameter( int diameter) {
+
+    public CellposeTaskSettings setDiameter(int diameter) {
         this.diameter = diameter;
         return this;
     }
@@ -57,25 +58,24 @@ public class CellposeTaskSettings {
         this.flow_threshold = flow_threshold;
         return this;
     }
+
     /*
     public CellposeTaskSettings setAnisotropy(double anisotropy) {
         this.anisotropy = anisotropy;
         return this;
     }
     */
-    public CellposeTaskSettings setCellProbTh( double cellprob_threshold) {
+    public CellposeTaskSettings setCellProbTh(double cellprob_threshold) {
         this.cellprob_threshold = cellprob_threshold;
         return this;
     }
 
-    public CellposeTaskSettings setFromPrefs(){
-        String keyPrefix = Cellpose.class.getName()+".";
-        this.useGpu = Prefs.get(keyPrefix + "useGpu"     , Cellpose.useGpu);
-        this.useMxnet = Prefs.get(keyPrefix + "useMxnet" , Cellpose.useMxnet);
+    public CellposeTaskSettings setFromPrefs() {
+        String keyPrefix = Cellpose.class.getName() + ".";
+        this.useGpu = Prefs.get(keyPrefix + "useGpu", Cellpose.useGpu);
+        this.useMxnet = Prefs.get(keyPrefix + "useMxnet", Cellpose.useMxnet);
         this.useFastMode = Prefs.get(keyPrefix + "useFastMode", Cellpose.useFastMode);
         this.useResample = Prefs.get(keyPrefix + "useResample", Cellpose.useResample);
         return this;
     }
-
-
 }
