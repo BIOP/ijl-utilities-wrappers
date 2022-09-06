60235a38ee23c278843f5e8cfba939ab7a08cb1f
romainGuiet <romain.guiet@epfl.ch>
2022-09-06 13:00:28 +0200

    adds cellpose_script.groovy

---
+++ resources/scripts/cellpose_script.groovy
@@ -0,0 +1,34 @@
+#@ImagePlus imp
+#@CommandService command
+#@Output labels
+#@RoiManager rm
+#@ResultsTable rt
+
+rt.reset()
+rm.reset()
+IJ.runMacro("close('\\\\Others');")
+
+// cellpose parameters
+def cellCellpose = new Cellpose_SegmentImgPlusOwnModelAdvanced();
+cellCellpose.imp = imp;
+cellCellpose.diameter = 50;
+//cellCellpose.cellproba_threshold = 0;
+//cellCellpose.flow_threshold = 0.4;
+cellCellpose.model = "cyto2";
+cellCellpose.nuclei_channel = 2 ;
+cellCellpose.cyto_channel = 1 ;
+cellCellpose.dimensionMode  = "2D";
+// cellpose run
+cellCellpose.run();
+// get the output labels image
+cell_cellpose_imp = cellCellpose.cellpose_imp ;
+cell_cellpose_imp.show()
+
+IJ.run(cell_cellpose_imp, "Label image to ROIs", "");
+IJ.run(cell_cellpose_imp, "glasbey_inverted", "");
+cell_cellpose_imp.setDisplayRange(0, 12);
+
+return
+
+import ch.epfl.biop.wrappers.cellpose.ij2commands.Cellpose_SegmentImgPlusOwnModelAdvanced
+import ij.IJ;
