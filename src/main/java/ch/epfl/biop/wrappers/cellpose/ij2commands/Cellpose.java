79bfaddfc7a92f6ed115ed8549bc29b5c8b3513c
romainGuiet <romain.guiet@epfl.ch>
2024-04-15 10:48:27 +0200

    rename commands

--- src/main/java/ch/epfl/biop/wrappers/cellpose/ij2commands/Cellpose.java
+++ src/main/java/ch/epfl/biop/wrappers/cellpose/ij2commands/Cellpose.java
@@ -1,7 +1,7 @@
-package ch.epfl.biop.wrappers.omnipose.ij2commands;
+package ch.epfl.biop.wrappers.cellpose.ij2commands;
 
-import ch.epfl.biop.wrappers.omnipose.OmniposeTaskSettings;
-import ch.epfl.biop.wrappers.omnipose.DefaultOmniposeTask;
+import ch.epfl.biop.wrappers.cellpose.CellposeTaskSettings;
+import ch.epfl.biop.wrappers.cellpose.DefaultCellposeTask;
 
 import ij.IJ;
 import ij.ImagePlus;
@@ -25,16 +25,15 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-
-@Plugin(type = Command.class, menuPath = "Plugins>BIOP>Cellpose/Omnipose> Omnipose ...")
-public class Omnipose_SegmentImgPlusOwnModelAdvanced implements Command {
+@Plugin(type = Command.class, menuPath = "Plugins>BIOP>Cellpose/Omnipose> Cellpose ...")
+public class Cellpose implements Command {
     static {
         if (IJ.isLinux()) {
-            default_conda_env_path = "/home/biop/conda/envs/omnipose"; // to ease setting on biop-desktop    }
+            default_conda_env_path = "/home/biop/conda/envs/cellpose"; // to ease setting on biop-desktop    }
         } else if (IJ.isWindows()) {
-            default_conda_env_path = "C:/Users/username/.conda/envs/omnipose";
+            default_conda_env_path = "C:/Users/username/.conda/envs/cellpose";
         } else if (IJ.isMacOSX()) {
-            default_conda_env_path = "/Users/username/.conda/envs/omnipose";
+            default_conda_env_path = "/Users/username/.conda/envs/cellpose";
         }
     }
 
@@ -49,18 +48,18 @@ public class Omnipose_SegmentImgPlusOwnModelAdvanced implements Command {
     @Parameter (visibility=ItemVisibility.MESSAGE)
     String message = "You can use the pretrained model, specify the model name below";
     @Parameter(label = "--pretrained_model" )
-    String model = "cyto2_omni" ;
+    String model = "cyto2" ;
 
     @Parameter (visibility=ItemVisibility.MESSAGE)
     String message0 ="You can access the list of models by clicking on the button below.";
 
-    @Parameter( label="List of omnipose models", callback="openModelsPage")
+    @Parameter( label="List of cellpose models", callback="openModelsPage")
     private Button openModelsPage;
 
     @Parameter (visibility=ItemVisibility.MESSAGE)
     String message1 = "OR To use your own model, specify the path below AND leave --pretrained_model empty";
     @Parameter(required = false, label = "model_path")
-    File model_path = new File("path/to/own_omnipose_model");
+    File model_path = new File("path/to/own_cellpose_model");
 
     // value defined from https://omnipose.readthedocs.io/en/latest/api.html
     @Parameter(label = "--diameter")
@@ -76,7 +75,7 @@ public class Omnipose_SegmentImgPlusOwnModelAdvanced implements Command {
     String message2 = "You can add more flags to the command line by adding them here. For example: --omni, --cluster";
 
     @Parameter(required = false, label = "To add more parameters (use comma separated list of flags)")
-    String additional_flags = "--omni, --cluster";
+    String additional_flags = "--use_gpu, --do_3D";
 
     @Parameter (visibility=ItemVisibility.MESSAGE)
     String message3 ="You can access the full list of parameters by clicking on the button below.";
@@ -85,7 +84,7 @@ public class Omnipose_SegmentImgPlusOwnModelAdvanced implements Command {
     private Button CliPageButton;
 
     @Parameter(type = ItemIO.OUTPUT)
-    ImagePlus omnipose_imp;
+    ImagePlus cellpose_imp;
 
     // necessary to open the cli page
     @Parameter
@@ -96,7 +95,7 @@ public class Omnipose_SegmentImgPlusOwnModelAdvanced implements Command {
     private void openCliPage() {
 
         try {
-            ps.open(new URL("https://omnipose.readthedocs.io/cli.html"));
+            ps.open(new URL("https://cellpose.readthedocs.io/en/latest/cli.html"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -106,7 +105,7 @@ public class Omnipose_SegmentImgPlusOwnModelAdvanced implements Command {
     private void openModelsPage() {
 
         try {
-            ps.open(new URL("https://omnipose.readthedocs.io/models.html"));
+            ps.open(new URL("https://cellpose.readthedocs.io/en/latest/models.html"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -116,21 +115,21 @@ public class Omnipose_SegmentImgPlusOwnModelAdvanced implements Command {
 
     @Override
     public void run() {
-        // Prepare omnipose settings
-        OmniposeTaskSettings settings = new OmniposeTaskSettings();
-        // and a omnipose task
-        DefaultOmniposeTask omniposeTask = new DefaultOmniposeTask();
+        // Prepare cellPose settings
+        CellposeTaskSettings settings = new CellposeTaskSettings();
+        // and a cellpose task
+        DefaultCellposeTask cellposeTask = new DefaultCellposeTask();
 
         Calibration cal = imp.getCalibration();
 
-        // We'll have the current time-point of the imp in a temp folder
+        // We'll ave the current time-point of the imp in a temp folder
         String tempDir = IJ.getDirectory("Temp");
         // create tempdir
-        File omniposeTempDir = new File(tempDir, "omniposeTemp");
-        omniposeTempDir.mkdir();
+        File cellposeTempDir = new File(tempDir, "cellposeTemp");
+        cellposeTempDir.mkdir();
 
         // when plugin crashes, image file can pile up in the folder, so we make sure to clear everything
-        File[] contents = omniposeTempDir.listFiles();
+        File[] contents = cellposeTempDir.listFiles();
         if (contents != null) {
             for (File f : contents) {
                 f.delete();
@@ -139,7 +138,7 @@ public class Omnipose_SegmentImgPlusOwnModelAdvanced implements Command {
 
         // Add it to the settings
         settings.setCondaEnvDir(conda_env_path.toString());
-        settings.setDatasetDir(omniposeTempDir.toString());
+        settings.setDatasetDir(cellposeTempDir.toString());
 
         if ( model==null || model.trim().equals("") ){
             System.out.println("Using custom model");
@@ -151,73 +150,73 @@ public class Omnipose_SegmentImgPlusOwnModelAdvanced implements Command {
         if (ch2 > -1 ) settings.setChannel2(ch2);
         settings.setAdditionalFlags(additional_flags);
 
-        // settings are done , so we can now process the imp with omnipose
-        omniposeTask.setSettings(settings);
+        // settings are done , so we can now process the imp with cellpose
+        cellposeTask.setSettings(settings);
         try {
             // can't process time-lapse directly so, we'll save one time-point after another
             int impFrames = imp.getNFrames();
 
             // we'll use list to store paths of saved input, output masks and outlines
             List<File> t_imp_paths = new ArrayList<>();
-            List<File> omnipose_masks_paths = new ArrayList<>();
-            List<File> omnipose_outlines_paths = new ArrayList<>();
+            List<File> cellpose_masks_paths = new ArrayList<>();
+            List<File> cellpose_outlines_paths = new ArrayList<>();
 
             for (int t_idx = 1; t_idx <= impFrames; t_idx++) {
                 // duplicate all channels and all z-slices for a defined time-point
                 ImagePlus t_imp = new Duplicator().run(imp, 1, imp.getNChannels(), 1, imp.getNSlices(), t_idx, t_idx);
-                // and save the current t_imp into the omniposeTempDir
-                File t_imp_path = new File(omniposeTempDir, imp.getShortTitle() + "-t" + t_idx + ".tif");
+                // and save the current t_imp into the cellposeTempDir
+                File t_imp_path = new File(cellposeTempDir, imp.getShortTitle() + "-t" + t_idx + ".tif");
                 FileSaver fs = new FileSaver(t_imp);
                 fs.saveAsTiff(t_imp_path.toString());
                 if (verbose) System.out.println(t_imp_path.toString());
                 // add to list of paths to delete at the end of operations
                 t_imp_paths.add(t_imp_path);
 
-                // prepare path of the omnipose mask output
-                File omnipose_imp_path = new File(omniposeTempDir, imp.getShortTitle() + "-t" + t_idx + "_cp_masks" + ".tif");
-                omnipose_masks_paths.add(omnipose_imp_path);
-                // omnipose also creates a txt file (probably to be used with a script to import ROI in imagej), we'll delete it too
+                // prepare path of the cellpose mask output
+                File cellpose_imp_path = new File(cellposeTempDir, imp.getShortTitle() + "-t" + t_idx + "_cp_masks" + ".tif");
+                cellpose_masks_paths.add(cellpose_imp_path);
+                // cellpose also creates a txt file (probably to be used with a script to import ROI in imagej), we'll delete it too
                 // (to generate ROIs from the label image we can use https://github.com/BIOP/ijp-larome)
-                File omnipose_outlines_path = new File(omniposeTempDir, imp.getShortTitle() + "-t" + t_idx + "_cp_outlines" + ".txt");
-                omnipose_outlines_paths.add(omnipose_outlines_path);
+                File cellpose_outlines_path = new File(cellposeTempDir, imp.getShortTitle() + "-t" + t_idx + "_cp_outlines" + ".txt");
+                cellpose_outlines_paths.add(cellpose_outlines_path);
             }
 
-            // RUN omnipose !
-            omniposeTask.run();
+            // RUN CELLPOSE !
+            cellposeTask.run();
 
-            // Open all the omnipose_mask and store each imp within an ArrayList
+            // Open all the cellpose_mask and store each imp within an ArrayList
             ArrayList<ImagePlus> imps = new ArrayList<>(impFrames);
             for (int t_idx = 1; t_idx <= impFrames; t_idx++) {
-                ImagePlus omnipose_t_imp = IJ.openImage(omnipose_masks_paths.get(t_idx - 1).toString());
+                ImagePlus cellpose_t_imp = IJ.openImage(cellpose_masks_paths.get(t_idx - 1).toString());
                 // make sure to make a 16-bit imp
                 // (issue with time-lapse, first frame have less than 254 objects and latest have more)
-                if (omnipose_t_imp.getBitDepth() != 32 ) {
-                    if (omnipose_t_imp.getNSlices() > 1) {
-                        omnipose_t_imp.getStack().setBitDepth(32);
+                if (cellpose_t_imp.getBitDepth() != 32 ) {
+                    if (cellpose_t_imp.getNSlices() > 1) {
+                        cellpose_t_imp.getStack().setBitDepth(32);
                     } else {
-                        omnipose_t_imp.setProcessor(omnipose_t_imp.getProcessor().convertToFloat());
+                        cellpose_t_imp.setProcessor(cellpose_t_imp.getProcessor().convertToFloat());
                     }
                 }
 
-                imps.add(omnipose_t_imp.duplicate());
+                imps.add(cellpose_t_imp.duplicate());
             }
             // Convert the ArrayList to an imp
             // https://stackoverflow.com/questions/9572795/convert-list-to-array-in-java
             ImagePlus[] impsArray = imps.toArray(new ImagePlus[0]);
-            omnipose_imp = Concatenator.run(impsArray);
-            omnipose_imp.setCalibration(cal);
-            omnipose_imp.setTitle(imp.getShortTitle() + "-omnipose");
+            cellpose_imp = Concatenator.run(impsArray);
+            cellpose_imp.setCalibration(cal);
+            cellpose_imp.setTitle(imp.getShortTitle() + "-cellpose");
 
             //add a LUT
-            IJ.run(omnipose_imp, "3-3-2 RGB", "");
+            IJ.run(cellpose_imp, "3-3-2 RGB", "");
 
             // Delete the created files and folder
             for (int t_idx = 1; t_idx <= impFrames; t_idx++) {
                 t_imp_paths.get(t_idx - 1).delete();
-                omnipose_masks_paths.get(t_idx - 1).delete();
-                omnipose_outlines_paths.get(t_idx - 1).delete();
+                cellpose_masks_paths.get(t_idx - 1).delete();
+                cellpose_outlines_paths.get(t_idx - 1).delete();
             }
-            omniposeTempDir.delete();
+            cellposeTempDir.delete();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -232,7 +231,7 @@ public class Omnipose_SegmentImgPlusOwnModelAdvanced implements Command {
         final ImageJ ij = new ImageJ();
         ij.ui().showUI();
         // will run on the current image
-        ij.command().run(Omnipose_SegmentImgPlusOwnModelAdvanced.class, true);
+        ij.command().run(Cellpose.class, true);
 
     }
 }
--- src/main/java/ch/epfl/biop/wrappers/omnipose/ij2commands/Omnipose.java
+++ src/main/java/ch/epfl/biop/wrappers/omnipose/ij2commands/Omnipose.java
@@ -1,7 +1,7 @@
-package ch.epfl.biop.wrappers.cellpose.ij2commands;
+package ch.epfl.biop.wrappers.omnipose.ij2commands;
 
-import ch.epfl.biop.wrappers.cellpose.CellposeTaskSettings;
-import ch.epfl.biop.wrappers.cellpose.DefaultCellposeTask;
+import ch.epfl.biop.wrappers.omnipose.OmniposeTaskSettings;
+import ch.epfl.biop.wrappers.omnipose.DefaultOmniposeTask;
 
 import ij.IJ;
 import ij.ImagePlus;
@@ -25,15 +25,16 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-@Plugin(type = Command.class, menuPath = "Plugins>BIOP>Cellpose/Omnipose> Cellpose ...")
-public class Cellpose_SegmentImgPlusOwnModelAdvanced implements Command {
+
+@Plugin(type = Command.class, menuPath = "Plugins>BIOP>Cellpose/Omnipose> Omnipose ...")
+public class Omnipose implements Command {
     static {
         if (IJ.isLinux()) {
-            default_conda_env_path = "/home/biop/conda/envs/cellpose"; // to ease setting on biop-desktop    }
+            default_conda_env_path = "/home/biop/conda/envs/omnipose"; // to ease setting on biop-desktop    }
         } else if (IJ.isWindows()) {
-            default_conda_env_path = "C:/Users/username/.conda/envs/cellpose";
+            default_conda_env_path = "C:/Users/username/.conda/envs/omnipose";
         } else if (IJ.isMacOSX()) {
-            default_conda_env_path = "/Users/username/.conda/envs/cellpose";
+            default_conda_env_path = "/Users/username/.conda/envs/omnipose";
         }
     }
 
@@ -48,18 +49,18 @@ public class Cellpose_SegmentImgPlusOwnModelAdvanced implements Command {
     @Parameter (visibility=ItemVisibility.MESSAGE)
     String message = "You can use the pretrained model, specify the model name below";
     @Parameter(label = "--pretrained_model" )
-    String model = "cyto2" ;
+    String model = "cyto2_omni" ;
 
     @Parameter (visibility=ItemVisibility.MESSAGE)
     String message0 ="You can access the list of models by clicking on the button below.";
 
-    @Parameter( label="List of cellpose models", callback="openModelsPage")
+    @Parameter( label="List of omnipose models", callback="openModelsPage")
     private Button openModelsPage;
 
     @Parameter (visibility=ItemVisibility.MESSAGE)
     String message1 = "OR To use your own model, specify the path below AND leave --pretrained_model empty";
     @Parameter(required = false, label = "model_path")
-    File model_path = new File("path/to/own_cellpose_model");
+    File model_path = new File("path/to/own_omnipose_model");
 
     // value defined from https://omnipose.readthedocs.io/en/latest/api.html
     @Parameter(label = "--diameter")
@@ -75,7 +76,7 @@ public class Cellpose_SegmentImgPlusOwnModelAdvanced implements Command {
     String message2 = "You can add more flags to the command line by adding them here. For example: --omni, --cluster";
 
     @Parameter(required = false, label = "To add more parameters (use comma separated list of flags)")
-    String additional_flags = "--use_gpu, --do_3D";
+    String additional_flags = "--omni, --cluster";
 
     @Parameter (visibility=ItemVisibility.MESSAGE)
     String message3 ="You can access the full list of parameters by clicking on the button below.";
@@ -84,7 +85,7 @@ public class Cellpose_SegmentImgPlusOwnModelAdvanced implements Command {
     private Button CliPageButton;
 
     @Parameter(type = ItemIO.OUTPUT)
-    ImagePlus cellpose_imp;
+    ImagePlus omnipose_imp;
 
     // necessary to open the cli page
     @Parameter
@@ -95,7 +96,7 @@ public class Cellpose_SegmentImgPlusOwnModelAdvanced implements Command {
     private void openCliPage() {
 
         try {
-            ps.open(new URL("https://cellpose.readthedocs.io/en/latest/cli.html"));
+            ps.open(new URL("https://omnipose.readthedocs.io/cli.html"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -105,7 +106,7 @@ public class Cellpose_SegmentImgPlusOwnModelAdvanced implements Command {
     private void openModelsPage() {
 
         try {
-            ps.open(new URL("https://cellpose.readthedocs.io/en/latest/models.html"));
+            ps.open(new URL("https://omnipose.readthedocs.io/models.html"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -115,21 +116,21 @@ public class Cellpose_SegmentImgPlusOwnModelAdvanced implements Command {
 
     @Override
     public void run() {
-        // Prepare cellPose settings
-        CellposeTaskSettings settings = new CellposeTaskSettings();
-        // and a cellpose task
-        DefaultCellposeTask cellposeTask = new DefaultCellposeTask();
+        // Prepare omnipose settings
+        OmniposeTaskSettings settings = new OmniposeTaskSettings();
+        // and a omnipose task
+        DefaultOmniposeTask omniposeTask = new DefaultOmniposeTask();
 
         Calibration cal = imp.getCalibration();
 
-        // We'll ave the current time-point of the imp in a temp folder
+        // We'll have the current time-point of the imp in a temp folder
         String tempDir = IJ.getDirectory("Temp");
         // create tempdir
-        File cellposeTempDir = new File(tempDir, "cellposeTemp");
-        cellposeTempDir.mkdir();
+        File omniposeTempDir = new File(tempDir, "omniposeTemp");
+        omniposeTempDir.mkdir();
 
         // when plugin crashes, image file can pile up in the folder, so we make sure to clear everything
-        File[] contents = cellposeTempDir.listFiles();
+        File[] contents = omniposeTempDir.listFiles();
         if (contents != null) {
             for (File f : contents) {
                 f.delete();
@@ -138,7 +139,7 @@ public class Cellpose_SegmentImgPlusOwnModelAdvanced implements Command {
 
         // Add it to the settings
         settings.setCondaEnvDir(conda_env_path.toString());
-        settings.setDatasetDir(cellposeTempDir.toString());
+        settings.setDatasetDir(omniposeTempDir.toString());
 
         if ( model==null || model.trim().equals("") ){
             System.out.println("Using custom model");
@@ -150,73 +151,73 @@ public class Cellpose_SegmentImgPlusOwnModelAdvanced implements Command {
         if (ch2 > -1 ) settings.setChannel2(ch2);
         settings.setAdditionalFlags(additional_flags);
 
-        // settings are done , so we can now process the imp with cellpose
-        cellposeTask.setSettings(settings);
+        // settings are done , so we can now process the imp with omnipose
+        omniposeTask.setSettings(settings);
         try {
             // can't process time-lapse directly so, we'll save one time-point after another
             int impFrames = imp.getNFrames();
 
             // we'll use list to store paths of saved input, output masks and outlines
             List<File> t_imp_paths = new ArrayList<>();
-            List<File> cellpose_masks_paths = new ArrayList<>();
-            List<File> cellpose_outlines_paths = new ArrayList<>();
+            List<File> omnipose_masks_paths = new ArrayList<>();
+            List<File> omnipose_outlines_paths = new ArrayList<>();
 
             for (int t_idx = 1; t_idx <= impFrames; t_idx++) {
                 // duplicate all channels and all z-slices for a defined time-point
                 ImagePlus t_imp = new Duplicator().run(imp, 1, imp.getNChannels(), 1, imp.getNSlices(), t_idx, t_idx);
-                // and save the current t_imp into the cellposeTempDir
-                File t_imp_path = new File(cellposeTempDir, imp.getShortTitle() + "-t" + t_idx + ".tif");
+                // and save the current t_imp into the omniposeTempDir
+                File t_imp_path = new File(omniposeTempDir, imp.getShortTitle() + "-t" + t_idx + ".tif");
                 FileSaver fs = new FileSaver(t_imp);
                 fs.saveAsTiff(t_imp_path.toString());
                 if (verbose) System.out.println(t_imp_path.toString());
                 // add to list of paths to delete at the end of operations
                 t_imp_paths.add(t_imp_path);
 
-                // prepare path of the cellpose mask output
-                File cellpose_imp_path = new File(cellposeTempDir, imp.getShortTitle() + "-t" + t_idx + "_cp_masks" + ".tif");
-                cellpose_masks_paths.add(cellpose_imp_path);
-                // cellpose also creates a txt file (probably to be used with a script to import ROI in imagej), we'll delete it too
+                // prepare path of the omnipose mask output
+                File omnipose_imp_path = new File(omniposeTempDir, imp.getShortTitle() + "-t" + t_idx + "_cp_masks" + ".tif");
+                omnipose_masks_paths.add(omnipose_imp_path);
+                // omnipose also creates a txt file (probably to be used with a script to import ROI in imagej), we'll delete it too
                 // (to generate ROIs from the label image we can use https://github.com/BIOP/ijp-larome)
-                File cellpose_outlines_path = new File(cellposeTempDir, imp.getShortTitle() + "-t" + t_idx + "_cp_outlines" + ".txt");
-                cellpose_outlines_paths.add(cellpose_outlines_path);
+                File omnipose_outlines_path = new File(omniposeTempDir, imp.getShortTitle() + "-t" + t_idx + "_cp_outlines" + ".txt");
+                omnipose_outlines_paths.add(omnipose_outlines_path);
             }
 
-            // RUN CELLPOSE !
-            cellposeTask.run();
+            // RUN omnipose !
+            omniposeTask.run();
 
-            // Open all the cellpose_mask and store each imp within an ArrayList
+            // Open all the omnipose_mask and store each imp within an ArrayList
             ArrayList<ImagePlus> imps = new ArrayList<>(impFrames);
             for (int t_idx = 1; t_idx <= impFrames; t_idx++) {
-                ImagePlus cellpose_t_imp = IJ.openImage(cellpose_masks_paths.get(t_idx - 1).toString());
+                ImagePlus omnipose_t_imp = IJ.openImage(omnipose_masks_paths.get(t_idx - 1).toString());
                 // make sure to make a 16-bit imp
                 // (issue with time-lapse, first frame have less than 254 objects and latest have more)
-                if (cellpose_t_imp.getBitDepth() != 32 ) {
-                    if (cellpose_t_imp.getNSlices() > 1) {
-                        cellpose_t_imp.getStack().setBitDepth(32);
+                if (omnipose_t_imp.getBitDepth() != 32 ) {
+                    if (omnipose_t_imp.getNSlices() > 1) {
+                        omnipose_t_imp.getStack().setBitDepth(32);
                     } else {
-                        cellpose_t_imp.setProcessor(cellpose_t_imp.getProcessor().convertToFloat());
+                        omnipose_t_imp.setProcessor(omnipose_t_imp.getProcessor().convertToFloat());
                     }
                 }
 
-                imps.add(cellpose_t_imp.duplicate());
+                imps.add(omnipose_t_imp.duplicate());
             }
             // Convert the ArrayList to an imp
             // https://stackoverflow.com/questions/9572795/convert-list-to-array-in-java
             ImagePlus[] impsArray = imps.toArray(new ImagePlus[0]);
-            cellpose_imp = Concatenator.run(impsArray);
-            cellpose_imp.setCalibration(cal);
-            cellpose_imp.setTitle(imp.getShortTitle() + "-cellpose");
+            omnipose_imp = Concatenator.run(impsArray);
+            omnipose_imp.setCalibration(cal);
+            omnipose_imp.setTitle(imp.getShortTitle() + "-omnipose");
 
             //add a LUT
-            IJ.run(cellpose_imp, "3-3-2 RGB", "");
+            IJ.run(omnipose_imp, "3-3-2 RGB", "");
 
             // Delete the created files and folder
             for (int t_idx = 1; t_idx <= impFrames; t_idx++) {
                 t_imp_paths.get(t_idx - 1).delete();
-                cellpose_masks_paths.get(t_idx - 1).delete();
-                cellpose_outlines_paths.get(t_idx - 1).delete();
+                omnipose_masks_paths.get(t_idx - 1).delete();
+                omnipose_outlines_paths.get(t_idx - 1).delete();
             }
-            cellposeTempDir.delete();
+            omniposeTempDir.delete();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -231,7 +232,7 @@ public class Cellpose_SegmentImgPlusOwnModelAdvanced implements Command {
         final ImageJ ij = new ImageJ();
         ij.ui().showUI();
         // will run on the current image
-        ij.command().run(Cellpose_SegmentImgPlusOwnModelAdvanced.class, true);
+        ij.command().run(Omnipose.class, true);
 
     }
 }
