ce51821b8663e088ab22fb8ed21924ce97b832bf
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2025-05-26 11:28:59 +0200

    attempt to debug build sh

--- .github/build.sh
+++ .github/build.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+set -ex
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-build.sh
-sh ci-build.sh
+ls -l ci-build.sh
+cat ci-build.sh | head -20
+bash ci-build.sh
