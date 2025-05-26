6dd141c4da1e656af494a8c60fc7c6a0232685f1
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2025-05-26 13:44:25 +0200

    Puts demo with a single plane to make things faster. Also testing float diameter

--- src/test/java/DemoCellpose.java
+++ src/test/java/DemoCellpose.java
@@ -22,7 +22,7 @@ public class DemoCellpose {
         IJ.run("Record...");
 
         //ImagePlus imp = new ImagePlus("src/test/resources/20191004_R03-C05-F03-crop.tif");
-        ImagePlus imp = IJ.openImage("src/test/resources/20191004_R03-C05-F03-crop.tif");
+        ImagePlus imp = IJ.openImage("src/test/resources/MAX_20191004_R03-C05-F03-crop.tif");
         imp.show();
 
         // Test 3D Cyto, 2chs
@@ -34,9 +34,9 @@ public class DemoCellpose {
     public static void cyto3D_2ch(ImageJ ij) throws ExecutionException, InterruptedException {
         ImagePlus imp = IJ.getImage();
         ImagePlus cytoLabel2chs_imp = (ImagePlus) ij.command().run(Cellpose.class, false,
-                "env_path", "C:\\Users\\chiarutt\\.conda\\envs\\cellpose", // "D:/conda/conda-envs/cellpose-307-gpu/", //
+                "env_path", "C:\\ProgramData\\miniforge3\\envs\\cellpose4",//"C:\\Users\\chiarutt\\.conda\\envs\\cellpose4", // "D:/conda/conda-envs/cellpose-307-gpu/", //
                 "imp", imp,
-                "diameter" , 45,
+                "diameter" , 45.5,
                 "model" , "cyto3",
                 "model_path","", // empty
                 "ch1",2,
