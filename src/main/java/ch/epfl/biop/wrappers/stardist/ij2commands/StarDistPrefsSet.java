94ea23cee947a1bb1ee764ec0bf91ea59b65b8bc
romainGuiet <romain.guiet@epfl.ch>
2025-11-21 12:21:07 +0100

    change default stardist default path, function of OS like in cellpose

--- src/main/java/ch/epfl/biop/wrappers/stardist/ij2commands/StarDistPrefsSet.java
+++ src/main/java/ch/epfl/biop/wrappers/stardist/ij2commands/StarDistPrefsSet.java
@@ -2,6 +2,7 @@ package ch.epfl.biop.wrappers.stardist.ij2commands;
 
 import ch.epfl.biop.wrappers.stardist.Stardist;
 
+import ij.IJ;
 import org.scijava.command.Command;
 import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
@@ -12,11 +13,22 @@ import java.io.File;
 @SuppressWarnings("CanBeFinal")
 @Plugin(type = Command.class, menuPath = "Plugins>BIOP>StarDist>StarDist3D setup...")
 public class StarDistPrefsSet implements Command {
+    static {
+        if (IJ.isLinux()) {
+            stardistEnvDirectory = "/opt/conda/envs/stardist"; // to ease setting on biop-desktop    }
+        } else if (IJ.isWindows()) {
+            stardistEnvDirectory = "C:/Users/username/.conda/envs/stardist";
+        } else if (IJ.isMacOSX()) {
+            stardistEnvDirectory = "/Users/username/.conda/envs/stardist";
+        }
+    }
+    static String stardistEnvDirectory;
+
     @Parameter
     LogService ls;
 
     @Parameter(style = "directory", persist = false)
-    File stardist_env_dir = new File(Stardist.stardistEnvDirectory);
+    File stardist_env_dir = new File( stardistEnvDirectory);
 
     @Parameter(choices = {"conda", "venv"}, persist = false)
     String env_type = Stardist.stardistEnvType;
