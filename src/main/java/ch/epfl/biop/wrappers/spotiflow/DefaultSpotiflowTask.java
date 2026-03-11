8330de5b5fa37dcf9497b3a694861bc6c7f2c66c
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2026-03-11 14:07:56 +0100

    WIP - appose spotiflow

--- src/main/java/ch/epfl/biop/wrappers/spotiflow/DefaultSpotiflowTask.java
+++ src/main/java/ch/epfl/biop/wrappers/spotiflow/DefaultSpotiflowTask.java
@@ -1,12 +1,24 @@
 package ch.epfl.biop.wrappers.spotiflow;
 
 import ch.epfl.biop.wrappers.ExecutePythonInConda;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class DefaultSpotiflowTask extends SpotiflowTask {
 
     @Override
     public void run() throws Exception {
+
+        try (InputStream is = getClass().getResourceAsStream("/ch/epfl/biop/wrappers/spotiflow/spotiflow.toml")) {
+            String content = IOUtils.toString(is, StandardCharsets.UTF_8);
+            System.out.println(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         String envPath = settings.envPath;
         String envType = settings.envType;
